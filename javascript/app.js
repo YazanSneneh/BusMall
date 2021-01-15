0420c0c4dbe1a5bf2efc690d3ffdc641cbbd1329
yazan <yaz.sneneh@gmail.com>
2021-01-16 01:41:18 +0200

    code refactored

--- javascript/app.js
+++ javascript/app.js
@@ -3,36 +3,31 @@ var leftProduct = document.getElementById('left-image');
 var midProduct = document.getElementById('mid-image');
 var rightProduct = document.getElementById('right-image');
 var resultButton = document.getElementById('result-btn');
-var container = document.getElementById('images-container')
+var container = document.getElementById('images-container');
 var resultContainer = document.getElementById('result-container');
 var barChart = document.getElementById('chart-canvas').getContext('2d');
-
-var productArray = []; // objects list
+var productArray = []; // products list
+var trackShownImages = [];
 var rounds = 25;   // user attempts times
 
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> model declrations 
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> data model, propreties and methods 
 function Product(name, path) {
     this.name = name;
     this.path = 'images/' + path;
     this.counter = 0;
-    this.imageAppearance = 0;
+    this.productAppearance = 0;
     productArray.push(this);
-
 }
 //  products functions
 function isClicked(product) {
     product.counter++;
-    console.log(` ${product.name} click times : ${product.counter}`)
+    setProductsToStorage()
     return product.counter
 };
 function timesShown(product) {
-    product.imageAppearance++;
-    // console.log(` ${product.name} appearance times : ${product.imageAppearance}`)
-    return product.imageAppearance;
-
+    product.productAppearance++;
+    return product.productAppearance;
 };
-
-// products
 new Product('bag', 'bag.jpg');
 new Product('banana', 'banana.jpg');
 new Product('bathroom', 'bathroom.jpg');
@@ -54,28 +49,49 @@ new Product('water-can', 'water-can.jpg');
 new Product('wine-glass', 'wine-glass.jpg');
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> functions declrations
-function pickProducts() {
-    var left = Math.floor(Math.random() * productArray.length);
-    var mid = Math.floor(Math.random() * productArray.length);
-    var right = Math.floor(Math.random() * productArray.length);
-    displayProducts(left, mid, right)
+function pickProducts() { // create randomly 3 products
+    do {  // check if number appeared last round
+        var left = Math.floor(Math.random() * productArray.length);
+    } while (checkShownProducts(left));
+
+    do { // check if left, right and mid uniqe and was not appeared last round
+        var mid = Math.floor(Math.random() * productArray.length);
+        var right = Math.floor(Math.random() * productArray.length);
+    } while (left === mid || left === right || mid === right || checkShownProducts(mid) || checkShownProducts(right))
+
+    // store data in array so later we ensure data dont appear next round 
+    trackShownImages = [];
+    trackShownImages = [left, mid, right];  // save numbers 
+    displayProducts(left, mid, right);
 }
-// add images to html
+
+function checkShownProducts(itemId) { // check if products appeard and don't pick it
+    for (var i = 0; i < trackShownImages.length; i++) {
+        if (itemId === trackShownImages[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+// add products to html
 function displayProducts(left, mid, right) {
     leftProduct.setAttribute('src', productArray[left].path);
+    leftProduct.setAttribute('alt', productArray[left].name);
     timesShown(productArray[left])
 
     midProduct.setAttribute('src', productArray[mid].path)
+    leftProduct.setAttribute('alt', productArray[mid].name);
     timesShown(productArray[mid])
 
     rightProduct.setAttribute('src', productArray[right].path)
+    leftProduct.setAttribute('alt', productArray[right].name);
     timesShown(productArray[right])
 }
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> event functions
 function trackUserPicks(event) {
     rounds--;    // user attempts
     if (rounds > 0) {
-        var userChoice = event.target
+        var userChoice = event.target;
         for (var i = 0; i < productArray.length; i++) {
             if (userChoice.getAttribute('src') === productArray[i].path) { // find image clicked, and count
                 isClicked(productArray[i])
@@ -86,27 +102,26 @@ function trackUserPicks(event) {
         container.removeEventListener('click', trackUserPicks);
         CreateChart()
     }
-    console.log(rounds)
 }
 
-function displayResultList() {  // display a report for each item
+function displayResultList() {  // display product List information
     var ul = document.createElement('ul');
     resultContainer.append(ul);
     for (var i = 0; i < productArray.length; i++) {
         var li = document.createElement('li');
-        li.textContent = productArray[i].name + ' had ' + productArray[i].counter + ' votes, and was seen ' + productArray[i].imageAppearance + ' times.';
+        li.textContent = productArray[i].name + ' had ' + productArray[i].counter + ' votes, and was seen ' + productArray[i].productAppearance + ' times.';
         ul.append(li);
     }
 }
 
-function CreateChart() {
-    var productsLabel = []
-    var productsClicked = []
-    var productsSeen = []
+function CreateChart() {  // present data visually using chart
+    var productsLabel = [];
+    var productsClicked = [];
+    var productsSeen = [];
     for (var i = 0; i < productArray.length; i++) {
         productsLabel.push(productArray[i].name)
         productsClicked.push(productArray[i].counter);
-        productsSeen.push(productArray[i].imageAppearance);
+        productsSeen.push(productArray[i].productAppearance);
     }
 
     var chart = new Chart(barChart, {
@@ -114,33 +129,34 @@ function CreateChart() {
         data: {
             labels: productsLabel,
             datasets: [{
-                label: '# Clicked',
+                label: ' Clicked Products',
                 data: productsClicked,
                 backgroundColor: '#ff884b',
                 borderColor: 'rgb(255, 99, 132)',
                 borderWidth: 1
             }, {
-                label: '# Seen',
+                label: 'Seen Products',
                 data: productsSeen,
                 backgroundColor: '#ff577f',
                 borderColor: 'rgb(255, 99, 132)',
                 borderWidth: 1
             }]
-        },
-
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
         }
     })
 }
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> executable code
+//  local Storage functions 
+function setProductsToStorage() {
+    localStorage.setItem('products', JSON.stringify(productArray));
+}
+function getProductsFromStorage() {
+    if (localStorage.length > 0) {
+        productArray = JSON.parse(localStorage.getItem('products'))
+    }
 
+}
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> executable code
 container.addEventListener('click', trackUserPicks)  // listen to images clicked
 resultButton.addEventListener('click', displayResultList) // listen to show results button
 pickProducts()   // display images
+getProductsFromStorage();
